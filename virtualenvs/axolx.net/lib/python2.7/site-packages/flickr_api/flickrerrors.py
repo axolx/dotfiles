966939760c87405150223451698ae12ec55ecfab
Martin Rio <martin@ombuweb.com>
2012-11-13 11:01:23 -0800

    removes virtualenvs

--- virtualenvs/axolx.net/lib/python2.7/site-packages/flickr_api/flickrerrors.py
+++
@@ -1,9 +0,0 @@
-
-class FlickrError(Exception):
-    pass
-
-class FlickrAPIError(FlickrError):
-    def __init__(self,code,message):
-        FlickrError.__init__(self,"%i : %s"%(code,message))
-        self.code = code
-        self.message = message
